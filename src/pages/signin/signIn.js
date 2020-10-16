a065ca217fb18aaaaddacaac0166e0e2bfc342e5
bwnstk <info@flimmerkiste.xyz>
2020-10-16 17:24:49 +0200

    Add super sick javascript for signIn and createAccount Page --- BOOOM ---

--- src/pages/signin/signIn.js
+++ src/pages/signin/signIn.js
@@ -1,12 +1,11 @@
 import { createElement } from "../../utils/elements";
-
-import { mailInput, passwordInput } from "../../components/input/input.stories";
+import { createButtonElement } from "../../components/button/button";
 import {
   signInButton,
   createAccountInactive,
 } from "../../components/button/button.stories";
 
-export function createSignIn() {
+export function createSignIn(inputs, title, button1, button2) {
   const forgotPassword = createElement("a", {
     className: "forgotPassword",
     innerText: "Forgot password?",
@@ -14,21 +13,21 @@ export function createSignIn() {
   });
   const pageHeading = createElement("h2", {
     className: "page-heading",
-    innerText: "Sign In",
+    innerText: title,
   });
 
   const form = createElement("form", {
     className: "inputForm",
-    children: [mailInput(), passwordInput()],
+    children: [...inputs],
   });
   const formContainer = createElement("div", {
     className: "formContainer",
-    children: [pageHeading, form, forgotPassword, signInButton()],
+    children: [pageHeading, form, forgotPassword, createButtonElement(button1)],
   });
 
   const pageContainer = createElement("div", {
     className: "pageContainer",
-    children: [formContainer, createAccountInactive()],
+    children: [formContainer, createButtonElement(button2)],
   });
 
   return pageContainer;
--- src/pages/signin/signIn.stories.js
+++ src/pages/signin/signIn.stories.js
@@ -1,6 +1,45 @@
 import "./signIn.css";
+import {
+  mailInput,
+  passwordInput,
+  nameInput,
+} from "../../components/input/input.stories";
+
+import rightArrow from "../../assets/icon-arrow-small.svg";
+
 import { createSignIn } from "./signIn.js";
 
+const signInList = [mailInput(), passwordInput()];
+const createAccountList = [mailInput(), passwordInput(), nameInput()];
+const buttonlist = [
+  {
+    type: "SIGN IN",
+    alt: "Arrow right",
+    imageSrc: rightArrow,
+  },
+  {
+    type: "CREATE ACCOUNT",
+    alt: "Arrow right",
+    imageSrc: rightArrow,
+  },
+  {
+    type: "CREATE ACCOUNT",
+    class: "btn--secondary",
+  },
+
+  {
+    type: "SIGN IN",
+    class: "btn--secondary",
+  },
+];
 export default { title: "Pages/Login" };
 
-export const signinScreen = () => createSignIn();
+export const signinScreen = () =>
+  createSignIn(signInList, "Sign In", buttonlist[0], buttonlist[2]);
+export const createAccountScreen = () =>
+  createSignIn(
+    createAccountList,
+    "Create Account",
+    buttonlist[1],
+    buttonlist[3]
+  );
